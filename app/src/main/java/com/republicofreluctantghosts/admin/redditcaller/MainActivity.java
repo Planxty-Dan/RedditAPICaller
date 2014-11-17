a2bab15a1996622a313c4995ed90dc15a799cc86
Planxty-Dan <dan.newport@detroitlabs.com>
2014-11-17 16:49:22 -0500

    setup textview, editText, and button in main.  Button launches next activity

--- app/src/main/java/com/republicofreluctantghosts/admin/redditcaller/MainActivity.java
+++ app/src/main/java/com/republicofreluctantghosts/admin/redditcaller/MainActivity.java
@@ -1,36 +1,40 @@
 package com.republicofreluctantghosts.admin.redditcaller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private final String SUBREDDIT_TAG = "subreddit name";
+    private TextView enterSubredditPrompt;
+    private EditText subredditEditText;
+    private Button submitSubreddtButton;
+    private String subredditName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        enterSubredditPrompt = (TextView) findViewById(R.id.enter_subreddit_prompt);
+        subredditEditText = (EditText) findViewById(R.id.subreddit_edittext);
+        submitSubreddtButton = (Button) findViewById(R.id.submit_subbreddit);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        submitSubreddtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                subredditName = subredditEditText.getText().toString().toLowerCase();
+                Intent intent = new Intent(getApplicationContext(), SubredditDisplayActivity.class);
+                intent.putExtra(SUBREDDIT_TAG, subredditName);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/republicofreluctantghosts/admin/redditcaller/SubredditDisplayActivity.java
@@ -0,0 +1,7 @@
+package com.republicofreluctantghosts.admin.redditcaller;
+
+/**
+ * Created by admin on 11/17/14.
+ */
+public class SubredditDisplayActivity {
+}
